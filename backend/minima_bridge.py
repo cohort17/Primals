81de780028f861a8d876a359631d6804d6203bf6
Eze Chiadikaobi <133181279+cohort17@users.noreply.github.com>
2025-08-21 22:18:09 +0100

    Create minima_bridge.py

---
+++ backend/minima_bridge.py
@@ -0,0 +1,12 @@
+import requests
+
+MINIMA_API = "http://127.0.0.1:9003"
+
+def lock_tokens_for_bridge(address, amount, tokenid, bridge_address):
+    params = {
+        "address": bridge_address,
+        "amount": amount,
+        "tokenid": tokenid,
+        "state": "LOCKED_FOR_BRIDGE"
+    }
+    return requests.post(MINIMA_API, json={"method": "send", **params}).json()
