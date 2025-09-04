a703ded1d9fe7e39c9cbdf2204ee180b36ff8556
Eze Chiadikaobi <133181279+cohort17@users.noreply.github.com>
2025-09-05 00:04:29 +0100

    Update minima_nft.py

--- backend/minima_nft.py
+++ backend/minima_nft.py
@@ -6,9 +6,9 @@ TOKEN_ID = "0xFA65DA403978B1E4B8A23FEA63BE27793660C1362000FF4042814C12911B1CCC"
 
 
 def is_valid_address(address):
-     return (isinstance(address, str) and
-     address.startswith("Mx") and
-     len(address) >= 30)
+     return ( isinstance(address, str) and
+             address.startswith("Mx") and
+             len(address) >= 30 )
 
 def is_valid_tokenid(tokenid):
     r = re.compile(r"^0x[a-fA-F0-9]{64}$")
